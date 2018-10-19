1965791e126922811145503527433a03291612b5
oscarricardosan <oscarricardosan@gmail.com>
2018-10-18 22:04:41 -0500

    Se agrega plugin para leer QR

--- www/js/pages/index.js
+++ www/js/pages/index.js
@@ -3,7 +3,29 @@ function initializePage(){
 
     $(document).bind("mobileinit", function(){
         $.mobile.allowCrossDomainPages = true;
-    });
+    });QRScanner.prepare(onDone); // show the prompt
+
+    function onDone(err, status){
+        if (err) {
+            // here we can handle errors and clean up any loose ends.
+            alert(err);
+        }
+        if (status.authorized) {
+            alert("autorizado");
+            // W00t, you have camera access and the scanner is initialized.
+            // QRscanner.show() should feel very fast.
+        } else if (status.denied) {
+            alert("denegado");
+            // The video preview will remain black, and scanning is disabled. We can
+            // try to ask the user to change their mind, but we'll have to send them
+            // to their device settings with `QRScanner.openSettings()`.
+        } else {
+            alert("denegado 2");
+            // we didn't get permission, but we didn't get permanently denied. (On
+            // Android, a denial isn't permanent unless the user checks the "Don't
+            // ask again" box. We can ask again at the next relevant opportunity.
+        }
+    }
 
     App= new Vue({
         el: '#App',
@@ -29,6 +51,24 @@ function initializePage(){
             },
 
             scan_qr: function(){
+                // Start a scan. Scanning will continue until something is detected or
+                // `QRScanner.cancelScan()` is called.
+                QRScanner.scan(displayContents);
+
+                function displayContents(err, text){
+                    if(err){
+                        alert(err);
+                        // an error occurred, or the scan was canceled (error code `6`)
+                    } else {
+                        // The scan completed, display the contents of the QR code:
+                        alert(text);
+                    }
+                }
+
+                // Make the webview transparent so the video preview is visible behind it.
+                QRScanner.show();
+
+                return;
                 cloudSky.zBar.scan({
                     text_title: "Leer código QR", // Android only
                     text_instructions: "Por favor apuntar tu camara al código QR", // Android only
